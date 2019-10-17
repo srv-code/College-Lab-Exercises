4cac531153783055cb71cf93c412bdca2623a395
Sourav Dey <sourav.blackjack@gmail.com>
2019-10-17 21:56:19 +0530

    Before 2nd sessional

---
+++ MCA/Sem 3/ASP/Lab/Class 11/Compact3/Comments.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Diagnostics;
+
+public partial class Comments : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e) {
+		Response.Write("user: " + Session["ename"].ToString());
+    }
+
+	protected void FormView1_DataBound(object sender, EventArgs e) {
+		Session["Iname"] = Convert.ToString(DataBinder.Eval(FormView1.DataItem, "ImageName"));
+
+		ControlCollection col = FormView1.Controls;
+		Debug.Print("Controls in FormView1:");
+		for (int i = 0; i < col.Count; i++) {
+			Debug.Print("  [{0}] {1}", i + 1, col[i]);
+		}
+
+		ListView ll = (ListView)FormView1.FindControl("ListView1");
+
+		ll.Visible = true;
+		ll.DataSourceID = "SqlDataSource2";
+		ll.DataBind();
+	}
+
+	protected void txtComment_TextChanged(object sender, EventArgs e) {
+		ListViewItem lt = (ListViewItem)((TextBox)sender).Parent;
+		TextBox cmt = (TextBox)lt.FindControl("txtComment");
+		SqlDataSource2.InsertParameters["iname"].DefaultValue = Session["Iname"].ToString();
+		// SqlDataSource2.InsertParameters["comment"].DefaultValue =((TextBox)sender).Text;
+		SqlDataSource2.InsertParameters["comment"].DefaultValue = cmt.Text;
+		SqlDataSource2.InsertParameters["ename"].DefaultValue = Session["ename"].ToString();
+		SqlDataSource2.Insert();
+	}
+}
---
+++ MCA/Sem 3/ASP/Lab/Class 11/Compact3/Login.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+public partial class Login : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e) {
+    }
+
+	protected void Button_Login_Click(object sender, EventArgs e) {
+		SqlDataReader sr;
+		SqlConnection MyConnection = new SqlConnection();
+		MyConnection.ConnectionString = ConfigurationManager.ConnectionStrings["ConnectionString2"].ConnectionString;
+		SqlCommand MyCommand = new SqlCommand();
+		MyCommand.CommandText = "SELECT * FROM emppp";
+		MyCommand.CommandType = CommandType.Text;
+		MyCommand.Connection = MyConnection;
+		MyCommand.Connection.Open();
+		sr = MyCommand.ExecuteReader(CommandBehavior.CloseConnection);
+		while (sr.Read()) {
+			if ((sr["ename"].ToString()) == TextBox_Username.Text && (sr["password"].ToString()) == TextBox_Password.Text) {
+				Session["ename"] = TextBox_Username.Text; Response.Redirect("~/Comments.aspx");
+			} else {
+				Response.Write("Authentication Failed");
+			}
+		}
+	}
+}
